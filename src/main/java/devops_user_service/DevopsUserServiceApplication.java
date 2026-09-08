ada0f91ba64ea641655294a50860bb8c204a0241
yuktha <yukthamuki@gmail.com>
2026-09-08 20:02:26 +0530

    first commit

---
+++ src/main/java/devops_user_service/DevopsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package devops_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devops_user_service/DevopsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package devops_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
